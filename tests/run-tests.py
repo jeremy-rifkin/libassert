251cd5ac93856c7f4410e84dc197c9f8a4d4d0e7
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-03-05 13:24:57 -0500

    Adding a test runner script and preliminary integration test outputs

---
+++ tests/run-tests.py
@@ -0,0 +1,56 @@
+import subprocess
+import os
+import sys
+
+ok = True
+
+def run_unit_tests(tests):
+	for test in tests:
+		print("[Running test bin/{}]".format(test))
+		p = subprocess.Popen(["bin/" + test + (".exe" if sys.platform == "win32" else "")])
+		p.wait(timeout=10)
+		if p.returncode != 0:
+			global ok
+			ok = False
+		print("[{}]".format("Passed" if p.returncode == 0 else "Failed"))
+
+def run_integration(expected_output_path):
+	print("[Running integration test against {}]".format(expected_output_path))
+	with open(expected_output_path) as f:
+		content = f.read().strip()
+	env = os.environ.copy()
+	lp = "{}/../bin".format(os.path.dirname(os.path.realpath(__file__)))
+	if "LD_LIBRARY_PATH" in env:
+		env["LD_LIBRARY_PATH"] += ":" + lp
+	else:
+		env["LD_LIBRARY_PATH"] = lp
+	p = subprocess.Popen(
+		["bin/integration" + (".exe" if sys.platform == "win32" else "")],
+		stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+	out, err = p.communicate()
+	out = out.decode("utf-8").strip()
+	if out != content:
+		with open("output.txt", "w") as f:
+			f.write(out)
+		dp = subprocess.Popen([
+			"icdiff",
+			expected_output_path,
+			"output.txt",
+			"--cols",
+			"100"
+		])
+		dp.wait()
+	elif p.returncode != 0 or len(err) != 0:
+		global ok
+		ok = False
+	print("[{}]".format("Passed" if p.returncode == 0 else "Failed"))
+
+def main():
+	assert(len(sys.argv) == 2)
+	run_unit_tests(["disambiguation", "literals"])
+	run_integration("integration/expected/{}.txt".format(sys.argv[1]))
+	global ok
+	print("tests " + ("passed" if ok else "failed"))
+	sys.exit(not ok)
+
+main()
