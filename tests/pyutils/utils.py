0cdeab208ff9a2c8914bb247d93f81d79d76a2d8
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-04-16 19:41:56 -0400

    changes to test runner to allow testing of optimized builds

--- tests/pyutils/utils.py
+++ tests/pyutils/utils.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import re
 import subprocess
@@ -47,6 +48,61 @@ def parse_output(output: str):
                 i += 1
             tokens.append(e)
     return tokens
+
+# This function scans two outputs and ignores close mismatches in line numbers
+def critical_difference(raw_output: str, raw_expected: str, max_line_diff: int, check_trace: bool = True):
+    try:
+        expected = parse_output(raw_expected)
+        output = parse_output(raw_output)
+        if len(output) != len(expected):
+            print("Note: critical_difference true due token count mismatch, {} vs {}"
+                .format(len(expected), len(output)), flush=True)
+            return True
+        for (a, b) in zip(expected, output):
+            if a["type"] != b["type"]:
+                print("Note: critical_difference true due to line mismatch in parsed trace blocks: {} vs {}"
+                    .format(a["type"], b["type"]), flush=True)
+                return True
+            if a["type"] == "sector" or a["type"] == "message":
+                if a["lines"] != b["lines"]:
+                    print("Note: critical_difference true due to line mismatch in message:")
+                    print("/" * 40)
+                    print(a["lines"])
+                    print("/" * 40 + " vs:")
+                    print(b["lines"])
+                    print("/" * 40, flush=True)
+                    return True
+            elif a["type"] == "trace":
+                if check_trace:
+                    for (line_a, line_b) in itertools.zip_longest(a["lines"], b["lines"]):
+                        if line_a != line_b:
+                            m_a = re.match(r"^(\s+.+):(\d+)$", line_a)
+                            m_b = re.match(r"^(\s+.+):(\d+)$", line_b)
+                            if m_a and m_b:
+                                if m_a.group(1) != m_b.group(1):
+                                    print("Note: critical_difference true due to file mismatch, {} vs {}"
+                                        .format(m_a.group(1), m_b.group(1)), flush=True)
+                                    return True
+                                l_a = int(m_a.group(2))
+                                l_b = int(m_b.group(2))
+                                if abs(l_a - l_b) > max_line_diff:
+                                    print("Note: critical_difference true due to line number mismatch, {} vs {}"
+                                        .format(l_a, l_b), flush=True)
+                                    return True
+                            else:
+                                print("Note: critical_difference true due to line mismatch in trace:")
+                                print("/" * 40)
+                                print(a["lines"])
+                                print("/" * 40 + " vs:")
+                                print(b["lines"])
+                                print("/" * 40, flush=True)
+                                return True
+            else:
+                assert(False)
+        return False
+    except Exception as e:
+        print("Exception in critical_difference:", e, flush=True)
+        return True
 
 def icdiff(a: Union[str, tuple], b: Union[str, tuple]):
     to_delete = []
@@ -101,3 +157,220 @@ def icdiff(a: Union[str, tuple], b: Union[str, tuple]):
         print("FileNotFoundError while spawning subprocess")
     for path in to_delete:
         os.remove(path)
+
+
+
+
+# Test cases
+
+def test_critical_difference(): # self test cases
+    max_line_diff = 2
+    # test equal
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(not critical_difference(a, a, max_line_diff))
+    # test message difference
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::somethfng_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(critical_difference(a, b, max_line_diff))
+    # test trace difference
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<char>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(critical_difference(a, b, max_line_diff))
+    # test non-critical line difference
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1410
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(not critical_difference(a, b, max_line_diff))
+    # test critical line difference
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1501
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(critical_difference(a, b, max_line_diff))
+    # test ignore-trace
+    a = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1501
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_asdfpp:1501
+# 2 void teasdfon.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(not critical_difference(a, b, max_line_diff, False))
+    # test section difference
+    a = """
+===================== [value printing: floating point] =====================
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    b = """
+===================== [value printing: flopping point] =====================
+Assertion failed at integration/integration.cpp:1408: void test_class<T>::something_else() [with T = int]:
+    assert(.1f == .1);
+    Where:
+        .1f => 0.100000001
+        .1  => 0.10000000000000001
+
+Stack trace:
+# 1 test_class<int>::something_else()
+      at integration.cpp:1408
+# 2 void test_class<int>::something<N>(std::pair<N, int>)
+      at integration.cpp:1005
+# 3 main
+      at integration.cpp:403
+""".strip()
+    assert(critical_difference(a, b, max_line_diff))
--- tests/run-tests.py
+++ tests/run-tests.py
@@ -5,7 +5,7 @@ import sys
 
 sys.stdout.reconfigure(encoding='utf-8') # for windows gh runner
 
-from pyutils.utils import parse_output, icdiff
+from pyutils.utils import critical_difference, test_critical_difference, icdiff, parse_output
 
 ok = True
 
@@ -29,59 +29,7 @@ def run_unit_tests(tests):
             ok = False
         print("[{}, code {}]".format("🟢 Passed" if p.returncode == 0 else "🔴 Failed", p.returncode), flush=True)
 
-# This function scans two outputs and ignores close mismatches in line numbers
-def critical_difference(output: str, expected_output: str): # TODO: Make use of utils.parse_output
-    try:
-        output_lines = [l.strip() for l in output.split("\n")]
-        expected_lines = [l.strip() for l in expected_output.split("\n")]
-        # state machine
-        in_trace = False
-        last_was_frame_line = False
-        if len(output_lines) != len(expected_lines):
-            print("(Note: critical_difference true due to lines mismatch, {} vs {})"
-                .format(len(expected_lines), len(output_lines)), flush=True)
-            return True
-        for i in range(len(output_lines)):
-            if in_trace:
-                if last_was_frame_line:
-                    last_was_frame_line = False
-                    l1 = output_lines[i].split(":")
-                    l2 = expected_lines[i].split(":")
-                    if l1[:-1] != l2[:-1]:
-                        print("(Note: critical_difference true due to trace file mismatch, {} vs {})"
-                            .format(repr(l2[:-1]), repr(l1[:-1])), flush=True)
-                        return True
-                    n1 = l1[-1]
-                    n2 = l2[-1]
-                    if n1 == "?" and n2 == "?":
-                        pass
-                    else:
-                        n1 = int(n1)
-                        n2 = int(n2)
-                        if abs(n1 - n2) > MAX_LINE_DIFF:
-                            print("(Note: critical_difference true due to line number mismatch, {} vs {})".format(n2, n1))
-                            return True
-                elif output_lines[i] != expected_lines[i]:
-                    print("(Note: critical_difference true due to line mismatch in trace, {} vs {})"
-                        .format(repr(expected_lines[i]), repr(output_lines[i])), flush=True)
-                    return True
-                elif output_lines[i] == "": # and expected_lines[i].strip() == "":
-                    in_trace = False
-                elif output_lines[i].startswith("# "): # and expected_lines[i].startswith("# "):
-                    last_was_frame_line = True
-            elif output_lines[i] != expected_lines[i]:
-                print("(Note: critical_difference true due to line mismatch, {} vs {})"
-                    .format(repr(expected_lines[i]), repr(output_lines[i])), flush=True)
-                return True
-            elif output_lines[i] == "Stack trace:": # and expected_lines[i] == "Stack trace:":
-                in_trace = True
-                last_was_frame_line = False
-        return False
-    except Exception as e:
-        print("Exception in critical_difference:", e)
-        return False
-
-def run_integration(expected_output_path: str):
+def run_integration(expected_output_path: str, opt: bool):
     print("[🔵 Running integration test against {}]".format(expected_output_path), flush=True)
     with open(expected_output_path) as f:
         expected_output = f.read()
@@ -91,15 +39,20 @@ def run_integration(expected_output_path: str):
     output, err = p.communicate()
     output = output.decode("utf-8").replace("\r", "")
     passed = True
-    if output != expected_output:
-        if critical_difference(output, expected_output):
+    expected_blocks = parse_output(expected_output)
+    output_blocks = parse_output(output)
+    if output_blocks != expected_blocks: # TODO for later: room for improvement for trace handling under opt
+        if critical_difference(output, expected_output, MAX_LINE_DIFF, not opt):
             passed = False
         else:
             print("WARNING: Difference in output but deemed non-critical", flush=True)
         print(os.path.basename(expected_output_path))
+        if opt:
+            expected_blocks = filter(lambda b: b["type"] != "trace", expected_blocks)
+            output_blocks = filter(lambda b: b["type"] != "trace", output_blocks)
         icdiff(
-            expected_output_path,
-            (output, "output.txt")
+            ("\n".join(map(lambda b: "\n".join(b["lines"]), expected_blocks)), "expected"),
+            ("\n".join(map(lambda b: "\n".join(b["lines"]), output_blocks)), "output")
         )
     elif p.returncode != 0:
         print("p.retruncode = {}".format(p.returncode), flush=True)
@@ -112,9 +65,16 @@ def run_integration(expected_output_path: str):
         ok = False
 
 def main():
-    assert(len(sys.argv) == 2)
+    assert(len(sys.argv) == 2 or len(sys.argv) == 3)
+    target = sys.argv[1]
+    opt = False
+    if len(sys.argv) == 3:
+        opt = sys.argv[2] == "opt"
+    if target == "self":
+        test_critical_difference()
+        return
     run_unit_tests(["disambiguation", "literals", "type_handling", "basic_test"])
-    run_integration("integration/expected/{}.txt".format(sys.argv[1]))
+    run_integration("integration/expected/{}.txt".format(target), opt)
     global ok
     print("Tests " + ("passed 🟢" if ok else "failed 🔴"), flush=True)
     sys.exit(not ok)
