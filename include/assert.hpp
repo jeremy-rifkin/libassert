eb4ade002d01d925b275a4fc2476c714af08f027
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-16 16:15:33 -0400

    try again

--- include/assert.hpp
+++ include/assert.hpp
@@ -1287,6 +1287,7 @@ using libassert::ASSERTION;
                 new (&libassert_decomposer) libassert::detail::expression_decomposer(std::move(libassert_r)); \
               } \
             } \
+        LIBASSERT_WARNING_PRAGMA_POP \
           }, \
           /* Note: std::launder needed in 17 in case of placement new / move shenanigans above */ \
           /* https://timsong-cpp.github.io/cppwp/n4659/basic.life#8.3 */ \
@@ -1294,7 +1295,6 @@ using libassert::ASSERTION;
           libassert::detail::get_expression_return_value <doreturn LIBASSERT_COMMA \
             libassert_ret_lhs LIBASSERT_COMMA std::is_lvalue_reference<decltype(libassert_value)>::value> \
               (libassert_value, *std::launder(&libassert_decomposer)); \
-        LIBASSERT_WARNING_PRAGMA_POP \
         ) LIBASSERT_IF(doreturn)(.value,)
 
 #ifdef NDEBUG
