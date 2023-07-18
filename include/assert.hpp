0c8043afa27e354044f06b6cfcacfefb7ebfa07c
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-18 08:16:45 -0400

    Indent

--- include/assert.hpp
+++ include/assert.hpp
@@ -298,26 +298,26 @@ namespace libassert::detail {
         LIBASSERT_GEN_OP_BOILERPLATE(bxor,   ^);
         LIBASSERT_GEN_OP_BOILERPLATE(bor,    |);
         #ifdef ASSERT_DECOMPOSE_BINARY_LOGICAL
-        struct land {
-            static constexpr std::string_view op_string = "&&";
-            template<typename A, typename B>
-            LIBASSERT_ATTR_COLD [[nodiscard]]
-            constexpr decltype(auto) operator()(A&& lhs, B&& rhs) const {
-                // Go out of the way to support old-style ASSERT(foo && "Message")
-                #if LIBASSERT_IS_GCC
-                if constexpr(is_string_literal<B>) {
-                    #pragma GCC diagnostic push
-                    #pragma GCC diagnostic ignored "-Wnonnull-compare"
-                    return std::forward<A>(lhs) == std::forward<B>(rhs);
-                    #pragma GCC diagnostic pop
-                } else {
-                    return std::forward<A>(lhs) == std::forward<B>(rhs);
-                }
-                #else
-                return std::forward<A>(lhs) == std::forward<B>(rhs);
-                #endif
-            }
-        };
+         struct land {
+             static constexpr std::string_view op_string = "&&";
+             template<typename A, typename B>
+             LIBASSERT_ATTR_COLD [[nodiscard]]
+             constexpr decltype(auto) operator()(A&& lhs, B&& rhs) const {
+                 // Go out of the way to support old-style ASSERT(foo && "Message")
+                 #if LIBASSERT_IS_GCC
+                  if constexpr(is_string_literal<B>) {
+                      #pragma GCC diagnostic push
+                      #pragma GCC diagnostic ignored "-Wnonnull-compare"
+                      return std::forward<A>(lhs) == std::forward<B>(rhs);
+                      #pragma GCC diagnostic pop
+                  } else {
+                      return std::forward<A>(lhs) == std::forward<B>(rhs);
+                  }
+                 #else
+                  return std::forward<A>(lhs) == std::forward<B>(rhs);
+                 #endif
+             }
+         };
          LIBASSERT_GEN_OP_BOILERPLATE(lor,    ||);
         #endif
         LIBASSERT_GEN_OP_BOILERPLATE(assign, =);
