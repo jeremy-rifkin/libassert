66ef6601cd82ac72bc73917accb3d480a94ddb73
doodspav <doodspav@gmail.com>
2023-11-01 20:06:14 +0000

    GHI #67 Add helper CMake files

---
+++ cmake/PreventInSourceBuilds.cmake
@@ -0,0 +1,8 @@
+# in-source build guard
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+    message(
+        FATAL_ERROR
+        "In-source builds are not supported. "
+        "You may need to delete 'CMakeCache.txt' and 'CMakeFiles/' before rebuilding this project."
+    )
+endif()
