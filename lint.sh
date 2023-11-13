ab0da61af13a3f3260f2cd0e35f97bc4fa40b809
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-11-13 13:20:59 -0500

    Fix lint CI

--- lint.sh
+++ lint.sh
@@ -4,11 +4,7 @@ status=0
 while read f
 do
     echo checking $f
-    flags=
-    if [ $f != "tests/unit/basic_test.cpp" ]; then
-        flags=-DASSERT_LOWERCASE
-    fi
-    clang-tidy $f -- -std=c++17 -Iinclude -Ibuild/_deps/cpptrace-src/include $flags
+    clang-tidy $f -p=build
     ret=$?
     if [ $ret -ne 0 ]; then
         status=1
