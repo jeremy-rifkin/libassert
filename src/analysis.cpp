93b514eb0cf6653876c9d6ae550016605c4ecb34
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-11-14 20:56:53 -0500

    Update dual abi handling and mingw test expected, turns out newer mingw doesn't have that issue anyway

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -66,7 +66,7 @@ namespace libassert::detail {
         replace_all_template(type, allocator);
         // replace std::__cxx11 -> std:: for gcc dual abi
         // https://gcc.gnu.org/onlinedocs/libstdc++/manual/using_dual_abi.html
-        replace_all_dynamic(type, "std::__cxx11", "std::");
+        replace_all_dynamic(type, "std::__cxx11::", "std::");
         return type;
     }
 
