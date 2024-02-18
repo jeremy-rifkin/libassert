e0e4fb45cab1ca17f93d2a4b2b652e34dbbe4957
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-02-18 17:59:36 -0600

    Another quick fix

--- tests/unit/test_public_utilities.cpp
+++ tests/unit/test_public_utilities.cpp
@@ -6,6 +6,16 @@
 
 using namespace libassert;
 
+std::string replace_all(std::string str, std::string_view substr, std::string_view replacement) {
+    std::string::size_type pos = 0;
+    // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-narrowing-conversions)
+    while((pos = str.find(substr.data(), pos, substr.length())) != std::string::npos) {
+        str.replace(pos, substr.length(), replacement.data(), replacement.length());
+        pos += replacement.length();
+    }
+    return str;
+}
+
 int main() {
     // pretty_type_name tests
     auto pretty_name = pretty_type_name<std::map<std::string, int>>();
@@ -14,5 +24,7 @@ int main() {
     // stringification tests
     DEBUG_ASSERT(generate_stringification(12) == "12");
     DEBUG_ASSERT(generate_stringification('x') == "'x'");
-    DEBUG_ASSERT(generate_stringification(std::make_pair("foobar", 20)) == R"(std::pair<const char*, int>: ["foobar", 20])");
+    DEBUG_ASSERT(
+        replace_all(generate_stringification(std::make_pair("foobar", 20)), "char *", "char*") == R"(std::pair<const char*, int>: ["foobar", 20])"
+    );
 }
