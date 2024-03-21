ee5f5e5bedf44abca7b02d8b5520e1547069230c
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-03-20 23:31:12 -0500

    Update catch2 integration to provide better output

--- include/libassert/assert-catch2.hpp
+++ include/libassert/assert-catch2.hpp
@@ -5,30 +5,12 @@
 #include <libassert/assert.hpp>
 
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers.hpp>
 
-class LibassertMatcher : public Catch::Matchers::MatcherBase<int> {
-    std::string message;
-
-public:
-    LibassertMatcher() = default;
-    LibassertMatcher(std::string message_) : message(std::move(message_)) {}
-
-    bool match(const int&) const override {
-        return message.empty();
-    }
-
-    virtual std::string describe() const override {
-        return message;
-    }
-};
-
-// #define CHECK(...) do { try { LIBASSERT_ASSERT(__VA_ARGS__); CHECK_THAT(1, LibassertMatcher()); } catch(std::exception& e) { CHECK_THAT(false, LibassertMatcher(e.what())); } } while(false)
-// #define REQUIRE(...) do { try { LIBASSERT_ASSERT(__VA_ARGS__); REQUIRE_THAT(1, LibassertMatcher()); } catch(std::exception& e) { REQUIRE_THAT(false, LibassertMatcher(e.what())); } } while(false)
 #if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL != 0
  #error "Libassert integration does not work with MSVC's non-conformant preprocessor. /Zc:preprocessor must be used."
 #endif
-#define ASSERT(...) do { try { LIBASSERT_ASSERT(__VA_ARGS__); REQUIRE_THAT(1, LibassertMatcher()); } catch(std::exception& e) { REQUIRE_THAT(false, LibassertMatcher(e.what())); } } while(false)
+// TODO: CHECK/REQUIRE?
+#define ASSERT(...) do { try { LIBASSERT_ASSERT(__VA_ARGS__); SUCCEED(); } catch(std::exception& e) { FAIL(e.what()); } } while(false)
 
 namespace libassert::detail {
     inline void catch2_failure_handler(const assertion_info& info) {
