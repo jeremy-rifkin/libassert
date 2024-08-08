c185bd7b3efb195f1e0f17286a24bb13d94cc85b
Sofie Finnes Øvrelid <sofie+git@steinwurf.com>
2024-08-08 11:33:37 +0200

    fix: ignore warning for already handled null pointer
    GCC 13.3 and GCC 14.1 both seem to incorrectly parse the stringification
    condition incorrectly, and will warn about null pointers that have
    already been returned early from.

--- include/libassert/stringification.hpp
+++ include/libassert/stringification.hpp
@@ -370,7 +370,10 @@ namespace libassert::detail {
                     return "nullptr";
                 }
             }
+            #pragma GCC diagnostic push
+            #pragma GCC diagnostic ignored "-Wnonnull"
             return stringification::stringify(std::string_view(v));
+            #pragma GCC diagnostic pop
         } else if constexpr(std::is_pointer_v<T> || std::is_function_v<T>) {
             return stringification::stringify_pointer_value(reinterpret_cast<const void*>(v));
         } else if constexpr(is_smart_pointer<T>) {
